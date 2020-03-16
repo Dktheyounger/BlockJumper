2db127baa8e542ea80dc2ff943a806ee3f84d9da
Daniel Kelsch <daniel.o.kelsch@gmail.com>
2020-03-16 01:24:00 -0600

    first commit

---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController controller;
+
+    [SerializeField]
+    private float speed = 12f;
+
+    [SerializeField]
+    private float speedInAir = 6f;
+
+    [SerializeField]
+    private float groundSpeed = 12;
+
+    [SerializeField]
+    private float jumpHeight = 3f;
+
+    [SerializeField]
+    private float gravity = -9.81f;
+
+    [SerializeField]
+    private Transform groundCheck;
+
+    [SerializeField]
+    private float groundDistance = 0.1f;
+
+    [SerializeField]
+    private LayerMask groundMask;
+
+    [SerializeField]
+    private AudioSource audioData;
+
+    private Vector3 velocity;
+    private bool isGround;
+
+    //Start is called once at the start
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Checking for ground
+        isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+       
+
+        //Checking if it should add gravity
+        if (isGround && velocity.y <0) 
+        {
+            velocity.y = -2f;
+        }
+
+        if (isGround != true)
+        {
+            speed = speedInAir;
+        }
+        else
+        {
+            speed = groundSpeed;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //Moving not gravity
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        //Jumping
+        if (Input.GetButtonDown("Jump") && isGround)
+        {
+            audioData.Play(0);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+       
+        //Gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        //Moving with gravity
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Pool.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool : MonoBehaviour
+{
+    [SerializeField]
+    private int PoolSize = 5;
+    [SerializeField]
+    private GameObject Prefab;
+    [SerializeField]
+    private float spawnRate = 4f;
+
+    //Y
+    [SerializeField]
+    private float MinY = -1f;
+    [SerializeField]
+    private float MaxY = 3.5f;
+
+    //Z 
+    [SerializeField]
+    private float MinZ = -1f;
+    [SerializeField]
+    private float MaxZ = 3.5f;
+
+    //X
+    [SerializeField]
+    private float MinX = -1f;
+    [SerializeField]
+    private float MaxX = 3.5f;
+
+    [SerializeField]
+    private float yChange;
+
+    [SerializeField]
+    private AudioSource audioData;
+
+
+    private GameObject[] PoolMembers;
+    private Vector3 objectPoolPosition = new Vector3(-100f, -100f, -100f);
+    private float timeSinceLastSpawned;
+
+    private int currentPoolMemeber = 0;
+
+
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+        PoolMembers = new GameObject[PoolSize];
+
+        for (int i = 0; i < PoolSize; i++)
+        {
+            PoolMembers[i] = (GameObject)Instantiate(Prefab, objectPoolPosition, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        timeSinceLastSpawned += Time.deltaTime;
+        MinY += yChange;
+        MaxY += yChange;
+
+        if (timeSinceLastSpawned >= spawnRate)
+        {
+            audioData.Play(0);
+            timeSinceLastSpawned = 0;
+            float spawnYPosition = Random.Range(MinY, MaxY);
+            float spawnXPosition = Random.Range(MinX, MaxX);
+            float spawnZPosition = Random.Range(MinZ, MaxZ);
+            
+
+            PoolMembers[currentPoolMemeber].transform.position = new Vector3(spawnXPosition, spawnYPosition, spawnZPosition);
+            currentPoolMemeber++;
+
+            if (currentPoolMemeber >= PoolSize)
+            {
+                currentPoolMemeber = 0;
+            }
+            
+        }
+    }
+}
